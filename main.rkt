f2202a1945a362e57f2c490306d25551490aca3f
AlexKnauth <alexander@knauth.org>
2016-05-02 18:21:45 -0400

    add tests

--- main.rkt
+++ main.rkt
@@ -5,6 +5,9 @@
 (require racket/list
          racket/set)
 
+(module+ test
+  (require rackunit))
+
 ;; syntax-sloc : Syntax -> Natural
 (define (syntax-sloc stx)
   (set-count (source-lines stx (set))))
@@ -12,23 +15,102 @@
 ;; source-lines : Any (Setof Natural) -> (Setof Natural)
 (define (source-lines stx lines)
   (cond [(syntax? stx)
+         ; this is the only case that adds to the set of lines
          (source-lines (syntax-e stx) (set-add lines (syntax-line stx)))]
         [(empty? stx)
          lines]
         [(cons? stx)
+         ; this adds everything in the car, then everything in the cdr
          (source-lines (cdr stx) (source-lines (car stx) lines))]
         [(box? stx)
+         ; this adds everything in the contents of the box
          (source-lines (unbox stx) lines)]
         [(vector? stx)
+         ; or vector
          (for/fold ([lines lines])
                    ([val (in-vector stx)])
            (source-lines val lines))]
         [(hash? stx)
+         ; or hash
          (for/fold ([lines lines])
                    ([(key val) (in-hash stx)])
            (source-lines val (source-lines key lines)))]
         [(struct? stx)
+         ; or non-opaque struct, including prefab structs,
          (source-lines (struct->vector stx) lines)]
         [else
+         ; otherwise give up.
          lines]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module+ test
+
+  (check-equal? (syntax-sloc #'(this is one line))
+                1)
+
+  (check-equal? (syntax-sloc #'(this
+                                is
+                                four
+                                lines))
+                4)
+
+  (check-equal? (syntax-sloc #'(this
+                                is
+                                ;; even with a comment!
+                                four
+                                lines))
+                4)
+
+  (check-equal? (syntax-sloc #'(this
+                                is
+                                #;
+                                (not how
+                                     ever
+                                     many
+                                     lines
+                                     this
+                                     is
+                                     but
+                                     still)
+                                four
+                                lines))
+                4)
+
+  (check-equal? (syntax-sloc #'(define (syntax-sloc stx)
+                                 (set-count (source-lines stx (set)))))
+                2)
+
+  (check-equal? (syntax-sloc
+                 #'(define (source-lines stx lines)
+                     (cond [(syntax? stx)
+                            ; this is the only case that adds to the set of lines
+                            (source-lines (syntax-e stx) (set-add lines (syntax-line stx)))]
+                           [(empty? stx)
+                            lines]
+                           [(cons? stx)
+                            ; this adds everything in the car, then everything in the cdr
+                            (source-lines (cdr stx) (source-lines (car stx) lines))]
+                           [(box? stx)
+                            ; this adds everything in the contents of the box
+                            (source-lines (unbox stx) lines)]
+                           [(vector? stx)
+                            ; or vector
+                            (for/fold ([lines lines])
+                                      ([val (in-vector stx)])
+                              (source-lines val lines))]
+                           [(hash? stx)
+                            ; or hash
+                            (for/fold ([lines lines])
+                                      ([(key val) (in-hash stx)])
+                              (source-lines val (source-lines key lines)))]
+                           [(struct? stx)
+                            ; or non-opaque struct, including prefab structs,
+                            (source-lines (struct->vector stx) lines)]
+                           [else
+                            ; otherwise give up.
+                            lines])))
+                21)
+
+  )
 
