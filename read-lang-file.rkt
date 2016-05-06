a1395995c2222c5301a8e673648829d0faa29d02
AlexKnauth <alexander@knauth.org>
2016-05-06 09:14:54 -0400

    add lang-file? predicate

--- read-lang-file.rkt
+++ read-lang-file.rkt
@@ -1,8 +1,13 @@
 #lang racket/base
 
-(provide read-lang-file)
+(provide read-lang-file
+         lang-file?)
 
 (require syntax/modread)
+
+(module+ test
+  (require rackunit
+           racket/runtime-path))
 
 ;; read-lang-file : Path-String -> Syntax
 (define (read-lang-file path-string)
@@ -13,4 +18,25 @@
      (lambda () 
        (read-syntax (object-name port) port)))
     (close-input-port port)))
+
+;; private value eq? to itself
+(define read-language-fail (gensym 'read-language-fail))
+
+;; lang-file? : Path-String -> Syntax
+(define (lang-file? path-string)
+  (define port (open-input-file path-string))
+  (port-count-lines! port)
+  (begin0
+    (not (eq? (read-language port (λ () read-language-fail)) read-language-fail))
+    (close-input-port port)))
+
+(module+ test
+  (define-runtime-path read-lang-file.rkt "read-lang-file.rkt")
+  (define-runtime-path lang-file-sloc.rkt "lang-file-sloc.rkt")
+  (define-runtime-path README.md "README.md")
+  (check-true (lang-file? read-lang-file.rkt))
+  (check-true (lang-file? lang-file-sloc.rkt))
+  (check-false (lang-file? README.md))
+  (check-pred syntax? (read-lang-file read-lang-file.rkt))
+  (check-pred syntax? (read-lang-file lang-file-sloc.rkt)))
 
