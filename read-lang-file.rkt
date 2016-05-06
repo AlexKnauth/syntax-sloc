d3bc0cdd8266998621ef589a5770e79c39afd80e
AlexKnauth <alexander@knauth.org>
2016-05-06 14:31:08 -0400

    check file-exists? within lang-file?

--- read-lang-file.rkt
+++ read-lang-file.rkt
@@ -24,19 +24,24 @@
 
 ;; lang-file? : Path-String -> Syntax
 (define (lang-file? path-string)
-  (define port (open-input-file path-string))
-  (port-count-lines! port)
-  (begin0
-    (not (eq? (read-language port (λ () read-language-fail)) read-language-fail))
-    (close-input-port port)))
+  (cond
+    [(file-exists? path-string)
+     (define port (open-input-file path-string))
+     (port-count-lines! port)
+     (begin0
+       (not (eq? (read-language port (λ () read-language-fail)) read-language-fail))
+       (close-input-port port))]
+    [else #f]))
 
 (module+ test
   (define-runtime-path read-lang-file.rkt "read-lang-file.rkt")
   (define-runtime-path lang-file-sloc.rkt "lang-file-sloc.rkt")
   (define-runtime-path README.md "README.md")
+  (define-runtime-path scribblings "scribblings")
   (check-true (lang-file? read-lang-file.rkt))
   (check-true (lang-file? lang-file-sloc.rkt))
   (check-false (lang-file? README.md))
+  (check-false (lang-file? scribblings))
   (check-pred syntax? (read-lang-file read-lang-file.rkt))
   (check-pred syntax? (read-lang-file lang-file-sloc.rkt)))
 
