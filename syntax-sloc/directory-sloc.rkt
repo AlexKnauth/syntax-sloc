6429431b7546cd23ba349b53d683b64744eb8599
AlexKnauth <alexander@knauth.org>
2016-05-11 19:14:51 -0400

    convert directory-sloc.rkt to typed racket

--- syntax-sloc/directory-sloc.rkt
+++ syntax-sloc/directory-sloc.rkt
@@ -1,11 +1,11 @@
-#lang racket/base
+#lang typed/racket/base
 
 (provide directory-sloc)
 
-(require syntax-sloc/lang-file-sloc syntax-sloc/read-lang-file)
+(require syntax-sloc/lang-file-sloc typed/syntax-sloc/read-lang-file)
 
-;; directory-sloc : Path-String [#:use-file? (Path -> Boolean)] -> Natural
+(: directory-sloc : Path-String [#:use-file? (Path -> Boolean)] -> Natural)
 (define (directory-sloc dir #:use-file? [use-file? (λ (path) #t)])
   (for/sum ([src (in-directory dir)]
-            #:when (and (file-exists? src) (use-file? src) (lang-file? src)))
+            #:when (and (file-exists? src) (use-file? src) (lang-file? src))) : Natural
     (lang-file-sloc src)))
