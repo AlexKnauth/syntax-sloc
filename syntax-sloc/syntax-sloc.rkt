9d5a75970065fbc38676e642cd8270a0d4a1ab3a
AlexKnauth <alexander@knauth.org>
2016-07-11 15:01:44 -0400

    add accumulate/stx/no-recur

--- syntax-sloc/syntax-sloc.rkt
+++ syntax-sloc/syntax-sloc.rkt
@@ -22,30 +22,39 @@
 
 (: accumulate/stx : (∀ (A) Any [A (Syntaxof Any) -> A] A -> A))
 (define (accumulate/stx stx add-stx acc)
+  (: add-stx* : A (Syntaxof Any) -> A)
+  (define (add-stx* acc stx)
+    (accumulate/stx/no-recur (syntax-e stx) add-stx* (add-stx acc stx)))
+  (accumulate/stx/no-recur stx add-stx* acc))
+
+(: accumulate/stx/no-recur : (∀ (A) Any [A (Syntaxof Any) -> A] A -> A))
+(define (accumulate/stx/no-recur stx add-stx acc)
   (cond [(syntax? stx)
          ; this is the only case that adds stx to acc
-         (accumulate/stx (syntax-e stx) add-stx (add-stx acc stx))]
+         (add-stx acc stx)]
         [(empty? stx)
          acc]
         [(cons? stx)
          ; this adds everything in the car, then everything in the cdr
-         (accumulate/stx (cdr stx) add-stx (accumulate/stx (car stx) add-stx acc))]
+         (accumulate/stx/no-recur (cdr stx) add-stx
+           (accumulate/stx/no-recur (car stx) add-stx acc))]
         [(box? stx)
          ; this adds everything in the contents of the box
-         (accumulate/stx (unbox stx) add-stx acc)]
+         (accumulate/stx/no-recur (unbox stx) add-stx acc)]
         [(vector? stx)
          ; or vector
          (for/fold ([acc acc])
                    ([val (in-vector stx)])
-           (accumulate/stx val add-stx acc))]
+           (accumulate/stx/no-recur val add-stx acc))]
         [(hash? stx)
          ; or hash
          (for/fold ([acc acc])
                    ([(key val) (in-hash stx)])
-           (accumulate/stx val add-stx (accumulate/stx key add-stx acc)))]
+           (accumulate/stx/no-recur val add-stx
+             (accumulate/stx/no-recur key add-stx acc)))]
         [(struct? stx)
          ; or non-opaque struct, including prefab structs,
-         (accumulate/stx (struct->vector stx) add-stx acc)]
+         (accumulate/stx/no-recur (struct->vector stx) add-stx acc)]
         [else
          ; otherwise give up.
          acc]))
@@ -95,35 +104,37 @@
                 2)
 
   (check-equal? (syntax-sloc
-                 #'(define (accumulate/stx stx add-stx acc)
+                 #'(define (accumulate/stx/no-recur stx add-stx acc)
                      (cond [(syntax? stx)
                             ; this is the only case that adds stx to acc
-                            (accumulate/stx (syntax-e stx) add-stx (add-stx acc stx))]
+                            (add-stx acc stx)]
                            [(empty? stx)
                             acc]
                            [(cons? stx)
                             ; this adds everything in the car, then everything in the cdr
-                            (accumulate/stx (cdr stx) add-stx (accumulate/stx (car stx) add-stx acc))]
+                            (accumulate/stx/no-recur (cdr stx) add-stx
+                              (accumulate/stx/no-recur (car stx) add-stx acc))]
                            [(box? stx)
                             ; this adds everything in the contents of the box
-                            (accumulate/stx (unbox stx) add-stx acc)]
+                            (accumulate/stx/no-recur (unbox stx) add-stx acc)]
                            [(vector? stx)
                             ; or vector
                             (for/fold ([acc acc])
                                       ([val (in-vector stx)])
-                              (accumulate/stx val add-stx acc))]
+                              (accumulate/stx/no-recur val add-stx acc))]
                            [(hash? stx)
                             ; or hash
                             (for/fold ([acc acc])
                                       ([(key val) (in-hash stx)])
-                              (accumulate/stx val add-stx (accumulate/stx key add-stx acc)))]
+                              (accumulate/stx/no-recur val add-stx
+                                (accumulate/stx/no-recur key add-stx acc)))]
                            [(struct? stx)
                             ; or non-opaque struct, including prefab structs,
-                            (accumulate/stx (struct->vector stx) add-stx acc)]
+                            (accumulate/stx/no-recur (struct->vector stx) add-stx acc)]
                            [else
                             ; otherwise give up.
                             acc])))
-                21)
+                23)
 
   )
 
