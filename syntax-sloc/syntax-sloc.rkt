6e93c3b531438d9f8feeb252c8fb81dc40d743de
AlexKnauth <alexander@knauth.org>
2016-07-11 16:00:58 -0400

    add syntax-sloc/filter

--- syntax-sloc/syntax-sloc.rkt
+++ syntax-sloc/syntax-sloc.rkt
@@ -12,9 +12,19 @@
 (define (syntax-sloc stx)
   (set-count (source-lines stx (set))))
 
+(: syntax-sloc/filter : (Syntaxof Any) [(Syntaxof Any) -> Boolean] -> Natural)
+(define (syntax-sloc/filter stx include-stx?)
+  (set-count (source-lines/filter stx include-stx? (set))))
+
 (: source-lines : Any (Setof Natural) -> (Setof Natural))
 (define (source-lines stx lines)
   (accumulate/stx* stx add-syntax-source-line lines))
+
+(: source-lines/filter : Any [(Syntaxof Any) -> Boolean] (Setof Natural) -> (Setof Natural))
+(define (source-lines/filter stx include-stx? lines)
+  (for/fold ([lines lines])
+            ([incl-stx (in-list (filter/stx stx include-stx?))])
+    (source-lines incl-stx lines)))
 
 (: add-syntax-source-line : (Setof Natural) (Syntaxof Any) -> (Setof Natural))
 (define (add-syntax-source-line lines stx)
@@ -114,38 +124,38 @@
                                  (set-count (source-lines stx (set)))))
                 2)
 
-  (check-equal? (syntax-sloc
-                 #'(define (accumulate/stx stx add-stx acc)
-                     (cond [(syntax? stx)
-                            ; this is the only case that adds stx to acc
-                            (add-stx acc stx)]
-                           [(empty? stx)
-                            acc]
-                           [(cons? stx)
-                            ; this adds everything in the car, then everything in the cdr
-                            (accumulate/stx (cdr stx) add-stx
-                              (accumulate/stx (car stx) add-stx acc))]
-                           [(box? stx)
-                            ; this adds everything in the contents of the box
-                            (accumulate/stx (unbox stx) add-stx acc)]
-                           [(vector? stx)
-                            ; or vector
-                            (for/fold ([acc acc])
-                                      ([val (in-vector stx)])
-                              (accumulate/stx val add-stx acc))]
-                           [(hash? stx)
-                            ; or hash
-                            (for/fold ([acc acc])
-                                      ([(key val) (in-hash stx)])
-                              (accumulate/stx val add-stx
-                                (accumulate/stx key add-stx acc)))]
-                           [(struct? stx)
-                            ; or non-opaque struct, including prefab structs,
-                            (accumulate/stx (struct->vector stx) add-stx acc)]
-                           [else
-                            ; otherwise give up.
-                            acc])))
-                23)
+  (define accumulate/stx-definition
+    #'(define (accumulate/stx stx add-stx acc)
+        (cond [(syntax? stx)
+               ; this is the only case that adds stx to acc
+               (add-stx acc stx)]
+              [(empty? stx)
+               acc]
+              [(cons? stx)
+               ; this adds everything in the car, then everything in the cdr
+               (accumulate/stx (cdr stx) add-stx
+                 (accumulate/stx (car stx) add-stx acc))]
+              [(box? stx)
+               ; this adds everything in the contents of the box
+               (accumulate/stx (unbox stx) add-stx acc)]
+              [(vector? stx)
+               ; or vector
+               (for/fold ([acc acc])
+                         ([val (in-vector stx)])
+                 (accumulate/stx val add-stx acc))]
+              [(hash? stx)
+               ; or hash
+               (for/fold ([acc acc])
+                         ([(key val) (in-hash stx)])
+                 (accumulate/stx val add-stx
+                   (accumulate/stx key add-stx acc)))]
+              [(struct? stx)
+               ; or non-opaque struct, including prefab structs,
+               (accumulate/stx (struct->vector stx) add-stx acc)]
+              [else
+               ; otherwise give up.
+               acc])))
+  (check-equal? (syntax-sloc accumulate/stx-definition) 23)
 
   (test-case "filter/stx"
     (define stx1 #'x)
@@ -162,6 +172,15 @@
                  (filter/stx (datum->syntax #f `#(,stx4 #(,stx5) ,stx6))
                              stx-list?)
                  (list stx4 stx5 stx6)))
+
+    (: for/fold-form? : (Syntaxof Any) -> Boolean)
+    (define (for/fold-form? stx)
+      (and
+       (stx-pair? stx)
+       (let ([a (stx-car stx)])
+         (and (identifier? a)
+              (free-identifier=? a #'for/fold)))))
+    (check-equal? (syntax-sloc/filter accumulate/stx-definition for/fold-form?) 7)
     )
 
   )
